ae769b5a2f26e060dca45cb828d0b088fa905def
罗富中 <luofuzhong@yixia.com>
2017-07-12 23:06:50 +0800

    some test

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // PdfConverter.convert(new File("1.pdf")).intoEpub("lfz test", new File("1.epub"));
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package java.pdf.converter;
+
+// import java.io.File;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // PdfConverter.convert(new File("1.pdf")).intoEpub("lfz test", new File("1.epub"));
+    }
+}
